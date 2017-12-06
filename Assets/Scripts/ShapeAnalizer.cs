a89a6450c79c3711269a1b28cf67e76ae3b0ceb1
Anton Simakov <ttohin@gmail.com>
2017-12-06 14:02:55 +0100

    Tuning shapes. Detect borders

--- Assets/Scripts/ShapeAnalizer.cs
+++ Assets/Scripts/ShapeAnalizer.cs
@@ -3,44 +3,44 @@ using MeshGeneration;
 using UnityEngine;
 
 namespace Komorki.Common {
-  public class Part {
-    public SquareBaseShape shape = SquareBaseShape.Empty;
-    public SquarePoint startPoint = SquarePoint.BottomLeft;
-  }
 
   public class ShapeAnalizer {
     public static int scale = 3;
     public ShapeAnalizer (Buffer<bool> inputBuffer, SquareGrid grid) {
 
       if (grid.width != inputBuffer.width * scale || grid.height != inputBuffer.height * scale)
-        throw new ArgumentException (string.Format ("grid should equal to inputBuffer * {}", scale));
+        throw new ArgumentException (string.Format ("grid should equal to inputBuffer * {0}", scale));
 
-      var internalBuffer = new Buffer<bool> (inputBuffer.width * scale, inputBuffer.height * scale);
-      inputBuffer.Scale (scale, out internalBuffer);
-
+      var internalBuffer = new Buffer<int> (inputBuffer.width * scale, inputBuffer.height * scale);
+      inputBuffer.ForEach ((value, i, j) => {
+        for (int iScale = 0; iScale < scale; ++iScale) {
+          for (int jScale = 0; jScale < scale; ++jScale) {
+            internalBuffer.Set (value ? 1 : 0, i * scale + iScale, j * scale + jScale);
+          }
+        }
+      });
       internalBuffer.ForEach ((value, x, y) => {
 
-        Part part = new Part ();
-        if (value == false) {
+        if (value == 0) {
           int nSquare = CountNeighborsSquare (internalBuffer, x, y);
           int nDiamond = CountNeighborsDiamond (internalBuffer, x, y);
 
           if (nSquare == 2 && nDiamond == 2) {
             AnalizeInternalTriagle (internalBuffer, grid.grid[x, y], x, y);
           } else if (nSquare == 2 && nDiamond == 3) {
-            AnalizeInternalTriagle (internalBuffer, grid.grid[x, y], x, y, 0.6f);
+            AnalizeInternalTriagle (internalBuffer, grid.grid[x, y], x, y, moveVertextToCorner: 0.6f);
           }
         } else {
           int nSquare = CountNeighborsSquare (internalBuffer, x, y);
           int nDiamond = CountNeighborsDiamond (internalBuffer, x, y);
           if (nSquare == 4) {
-            AnalizeSquare (internalBuffer, grid.grid[x, y], x, y);
+            AnalizeInternalSquare (internalBuffer, grid.grid[x, y], x, y);
           } else if (nSquare == 3 && nDiamond == 2) {
             AnalizeSquare (internalBuffer, grid.grid[x, y], x, y);
           } else if (nSquare == 3 && nDiamond == 3) {
-            AnalizeSquare (internalBuffer, grid.grid[x, y], x, y);
+            AnalizeInternalSquare (internalBuffer, grid.grid[x, y], x, y);
           } else if (nSquare == 2 && nDiamond == 2) {
-            AnalizeSquare (internalBuffer, grid.grid[x, y], x, y);
+            AnalizeInternalSquare (internalBuffer, grid.grid[x, y], x, y);
           } else if (nSquare == 2 && nDiamond == 1) {
             AnalizeTriagle (internalBuffer, grid.grid[x, y], x, y);
           }
@@ -48,40 +48,80 @@ namespace Komorki.Common {
       });
     }
 
-    int CountNeighborsSquare (Buffer<bool> buffer, int x, int y) {
-      bool value;
+    int CountNeighborsSquare (Buffer<int> buffer, int x, int y) {
+      int value;
       int count = 0;
-      if (buffer.Get (x + 1, y, out value) && value == true) count += 1;
-      if (buffer.Get (x - 1, y, out value) && value == true) count += 1;
-      if (buffer.Get (x, y + 1, out value) && value == true) count += 1;
-      if (buffer.Get (x, y - 1, out value) && value == true) count += 1;
+      if (buffer.Get (x + 1, y, out value) && value == 1) count += 1;
+      if (buffer.Get (x - 1, y, out value) && value == 1) count += 1;
+      if (buffer.Get (x, y + 1, out value) && value == 1) count += 1;
+      if (buffer.Get (x, y - 1, out value) && value == 1) count += 1;
       return count;
     }
 
-    int CountNeighborsDiamond (Buffer<bool> buffer, int x, int y) {
-      bool value;
+    int CountNeighborsDiamond (Buffer<int> buffer, int x, int y) {
+      int value;
       int count = 0;
-      if (buffer.Get (x + 1, y + 1, out value) && value == true) count += 1;
-      if (buffer.Get (x - 1, y - 1, out value) && value == true) count += 1;
-      if (buffer.Get (x - 1, y + 1, out value) && value == true) count += 1;
-      if (buffer.Get (x + 1, y - 1, out value) && value == true) count += 1;
+      if (buffer.Get (x + 1, y + 1, out value) && value == 1) count += 1;
+      if (buffer.Get (x - 1, y - 1, out value) && value == 1) count += 1;
+      if (buffer.Get (x - 1, y + 1, out value) && value == 1) count += 1;
+      if (buffer.Get (x + 1, y - 1, out value) && value == 1) count += 1;
       return count;
     }
 
-    void AnalizeSquare (Buffer<bool> buffer, Square square, int x, int y) {
+    void AnalizeSquare (Buffer<int> buffer, Square square, int x, int y) {
+      square.Init (SquarePoint.BottomLeft, SquareBaseShape.Square);
+
+      float moveVertexToBorder = 0.1f;
+      int value;
+      if (!buffer.Get (x - 1, y, out value) || value == 0) {
+        var point1 = SquarePoint.BottomLeft;
+        var point2 = SquarePoint.TopLeft;
+        var pointOffset1 = Vector3.left * moveVertexToBorder;
+        var pointOffset2 = Vector3.left * moveVertexToBorder;
+        var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
+        var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
+      } else if (!buffer.Get (x + 1, y, out value) || value == 0) {
+        var point1 = SquarePoint.BottomRight;
+        var point2 = SquarePoint.TopRight;
+        var pointOffset1 = Vector3.right * moveVertexToBorder;
+        var pointOffset2 = Vector3.right * moveVertexToBorder;
+        var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
+        var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
+      } else if (!buffer.Get (x, y - 1, out value) || value == 0) {
+        var point1 = SquarePoint.BottomLeft;
+        var point2 = SquarePoint.BottomRight;
+        var pointOffset1 = Vector3.down * moveVertexToBorder;
+        var pointOffset2 = Vector3.down * moveVertexToBorder;
+        var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
+        var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
+      } else if (!buffer.Get (x, y + 1, out value) || value == 0) {
+        var point1 = SquarePoint.TopLeft;
+        var point2 = SquarePoint.TopRight;
+        var pointOffset1 = Vector3.up * moveVertexToBorder;
+        var pointOffset2 = Vector3.up * moveVertexToBorder;
+        var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
+        var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
+      }
+    }
+
+    void AnalizeInternalSquare (Buffer<int> buffer, Square square, int x, int y) {
       square.Init (SquarePoint.BottomLeft, SquareBaseShape.Square);
     }
 
-    void AnalizeInternalSquare (Buffer<bool> buffer, Square square, int x, int y) {
-      square.Init (SquarePoint.BottomLeft, SquareBaseShape.Square);
-    }
-
-    void AnalizeTriagle (Buffer<bool> buffer, Square square, int x, int y, float moveVertextToCorner = 0.6f) {
+    void AnalizeTriagle (Buffer<int> buffer, Square square, int x, int y, float moveVertextToCorner = 0.6f) {
       SquarePoint startPoint = SquarePoint.Center;
       SquareBaseShape shape = SquareBaseShape.Triangle;
 
-      bool value;
-      if (buffer.Get (x, y - 1, out value) && value == true && buffer.Get (x + 1, y, out value) && value == true) {
+      int value;
+      if (buffer.Get (x, y - 1, out value) && value == 1 && buffer.Get (x + 1, y, out value) && value == 1) {
         startPoint = SquarePoint.BottomRight;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.BottomLeft;
@@ -90,11 +130,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.left * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
       }
 
-      if (buffer.Get (x, y + 1, out value) && value == true && buffer.Get (x - 1, y, out value) && value == true) {
+      if (buffer.Get (x, y + 1, out value) && value == 1 && buffer.Get (x - 1, y, out value) && value == 1) {
         startPoint = SquarePoint.TopLeft;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.BottomLeft;
@@ -103,10 +143,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.down * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
       }
-      if (buffer.Get (x, y - 1, out value) && value == true && buffer.Get (x - 1, y, out value) && value == true) {
+
+      if (buffer.Get (x, y - 1, out value) && value == 1 && buffer.Get (x - 1, y, out value) && value == 1) {
         startPoint = SquarePoint.BottomLeft;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.TopLeft;
@@ -115,10 +156,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.up * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
       }
-      if (buffer.Get (x, y + 1, out value) && value == true && buffer.Get (x + 1, y, out value) && value == true) {
+
+      if (buffer.Get (x, y + 1, out value) && value == 1 && buffer.Get (x + 1, y, out value) && value == 1) {
         startPoint = SquarePoint.TopRight;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.TopLeft;
@@ -127,17 +169,17 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.left * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
       }
-
     }
-    void AnalizeInternalTriagle (Buffer<bool> buffer, Square square, int x, int y, float moveVertextToCorner = 0.3f) {
+
+    void AnalizeInternalTriagle (Buffer<int> buffer, Square square, int x, int y, float moveVertextToCorner = 0.3f) {
       SquarePoint startPoint = SquarePoint.Center;
       SquareBaseShape shape = SquareBaseShape.Triangle;
 
-      bool value;
-      if (buffer.Get (x, y - 1, out value) && value == true && buffer.Get (x + 1, y, out value) && value == true) {
+      int value;
+      if (buffer.Get (x, y - 1, out value) && value == 1 && buffer.Get (x + 1, y, out value) && value == 1) {
         startPoint = SquarePoint.BottomRight;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.BottomLeft;
@@ -146,11 +188,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.down * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
       }
 
-      if (buffer.Get (x, y + 1, out value) && value == true && buffer.Get (x - 1, y, out value) && value == true) {
+      if (buffer.Get (x, y + 1, out value) && value == 1 && buffer.Get (x - 1, y, out value) && value == 1) {
         startPoint = SquarePoint.TopLeft;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.BottomLeft;
@@ -159,10 +201,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.left * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
       }
-      if (buffer.Get (x, y - 1, out value) && value == true && buffer.Get (x - 1, y, out value) && value == true) {
+
+      if (buffer.Get (x, y - 1, out value) && value == 1 && buffer.Get (x - 1, y, out value) && value == 1) {
         startPoint = SquarePoint.BottomLeft;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.TopLeft;
@@ -171,10 +214,11 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.left * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.y = pointPos1.y;
+        square.GetNode (point2).Vertex.Pos.x = pointPos2.x;
       }
-      if (buffer.Get (x, y + 1, out value) && value == true && buffer.Get (x + 1, y, out value) && value == true) {
+
+      if (buffer.Get (x, y + 1, out value) && value == 1 && buffer.Get (x + 1, y, out value) && value == 1) {
         startPoint = SquarePoint.TopRight;
         square.Init (startPoint, shape);
         var point1 = SquarePoint.TopLeft;
@@ -183,10 +227,9 @@ namespace Komorki.Common {
         var pointOffset2 = Vector3.up * moveVertextToCorner;
         var pointPos1 = square.GetAbsolutePosition (point1) + pointOffset1;
         var pointPos2 = square.GetAbsolutePosition (point2) + pointOffset2;
-        square.GetNode (point1).Vertex.Pos = pointPos1;
-        square.GetNode (point2).Vertex.Pos = pointPos2;
+        square.GetNode (point1).Vertex.Pos.x = pointPos1.x;
+        square.GetNode (point2).Vertex.Pos.y = pointPos2.y;
       }
-
     }
   }
 }
