3494a5607a686e59f78f252e828e0c93d91acb84
Anton Simakov <ttohin@gmail.com>
2017-12-21 20:50:14 +0100

    Improved DoF effect

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var d = Input.GetAxis("Mouse ScrollWheel");
+		var dofSettings = GetComponent<PostProcessingBehaviour> ().profile.depthOfField.settings;
+		dofSettings.focusDistance += d * 0.5f;
+		GetComponent<PostProcessingBehaviour> ().profile.depthOfField.settings = dofSettings;
+
+	}
+}
