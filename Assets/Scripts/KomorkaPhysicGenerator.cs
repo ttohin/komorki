c530d94d138d46ddd7602cba0440b3952c464144
Anton Simakov <ttohin@gmail.com>
2017-12-21 17:07:50 +0100

    added PostProcessing assets and configured simple Depth Of Field

--- Assets/Scripts/KomorkaPhysicGenerator.cs
+++ Assets/Scripts/KomorkaPhysicGenerator.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class KomorkaPhysicGenerator : MonoBehaviour {
 
-  private int InitialNumberOfKomorkas = 1000;
-  public GameObject komorkaPrefab;
+	private int InitialNumberOfKomorkas = 100;
+	public GameObject komorkaPrefab;
 
-  // Use this for initialization
-  void Start () {
-    for (int i = 0; i < InitialNumberOfKomorkas; i++) {
-      Instantiate (komorkaPrefab, new Vector3 (Random.Range (-40, 40), Random.Range (0, 14), Random.Range (-5, 5)), Quaternion.identity);
-    }
-  }
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < InitialNumberOfKomorkas; i++) {
+			Instantiate (komorkaPrefab, new Vector3 (Random.Range (-15, 15), Random.Range (0, 14), Random.Range (-2, 2)), Quaternion.identity);
+		}
+	}
 
   // Update is called once per frame
   void Update () {
